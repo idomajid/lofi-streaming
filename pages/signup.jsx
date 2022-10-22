33db5f305cde6c588a37cad76704a15092c74b1d
IDO Majid <idomajid99@gmail.com>
2022-10-22 13:10:48 +0700

    signup page

---
+++ pages/signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignUpCard from "../components/SignUpCard";
+
+export default function Signup() {
+  return (
+    <div>
+      <SignUpCard />
+    </div>
+  );
+}
