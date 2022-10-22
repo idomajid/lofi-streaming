9ac93c9841b3d410f6539d56c04aafbd0c81477d
IDO Majid <idomajid99@gmail.com>
2022-10-22 13:14:28 +0700

    testing streaming feature

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-import Signup from "../components/SignUp";
+import Signup from "../components/SignUpCard";
+import Login from "../components/LoginCard";
+import YoutubeEmbed from "../components/YoutubeEmbed";
 
 export default function Home() {
   return (
-    <div>
-      <Signup />
+    <div className={styles.container}>
+      <button>Create Account page</button>
+      <button>Login Page</button>
+      <div>
+        <h1>Youtube Embed</h1>
+        <YoutubeEmbed embedId="jfKfPfyJRdk" />
+      </div>
     </div>
   );
 }
