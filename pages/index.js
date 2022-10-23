64ec234130fd0181fce16cc695d4aa63a706684e
IDO Majid <idomajid99@gmail.com>
2022-10-23 16:01:28 +0700

    reamove Button streaming page and move into nav

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,30 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "../styles/Home.module.css";
 
-import Signup from "../components/SignUpCard";
-import Login from "../components/LoginCard";
-import YoutubeEmbed from "../components/YoutubeEmbed";
+import { useRouter } from "next/router";
+import styles from "../styles/Container.module.css";
+import Header from "../components/Header";
 
 export default function Home() {
   const router = useRouter();
 
   return (
     <div className={styles.container}>
-      <Link href="/signup">
-        <button>Create Account page</button>
-      </Link>
-      <Link href="/login">
-        <button>Login Page</button>
-      </Link>
-      <div>
-        <Link href="/streamingPage">
-          <button>Watch Streaming</button>
-        </Link>
+      <Header />
+      <div style={{ width: "100%", height: "100%" }}>
+        <Image
+          className="rounded-md"
+          src="/../public/doctor-before.jpg"
+          alt="Picture of the author"
+          width="1500px"
+          height="678px"
+        />
+        {/* <Link href="/streamingPage">
+          <button className="px-24 py-3 bg-blue-500 rounded text-white font-medium">
+            Go to streaming Page
+          </button>
+        </Link> */}
       </div>
     </div>
   );
