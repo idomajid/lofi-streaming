a7916a9be2fa311ff3c429809798c53fac34b09d
IDO Majid <idomajid99@gmail.com>
2022-10-23 16:00:13 +0700

    adding streaming page and signOut

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import supabase from "../pages/api/supabase";
 
 export default function Header() {
+  const onSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.log(`${error} from signOut`);
+    }
+  };
+
   return (
     <div className="flex flex-row justify-between h-24 items-center ">
       <div className="text-3xl antialiased font-medium">
@@ -16,6 +25,11 @@ export default function Header() {
         <Link href="/login">
           <button className="px-4 py-1 bg-blue-500 rounded text-white font-medium">
             Login
+          </button>
+        </Link>
+        <Link href="/streamingPage">
+          <button className="px-4 py-1 bg-gray-300 rounded text-white font-medium">
+            Go to Streaming page
           </button>
         </Link>
       </div>
