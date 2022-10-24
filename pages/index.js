cb8c66d8fd90c94d90ac5085330574ac7f04996e
IDO Majid <idomajid99@gmail.com>
2022-10-24 16:49:37 +0700

    responsive

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
-import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 
-import { useRouter } from "next/router";
 import styles from "../styles/Container.module.css";
 import Header from "../components/Header";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <div className={styles.container}>
       <Header />
-      <div style={{ width: "100%", height: "100%" }}>
+      <div className="py-4 md:py-0">
         <Image
           className="rounded-md"
           src="/../public/doctor-before.jpg"
@@ -20,11 +15,6 @@ export default function Home() {
           width="1500px"
           height="678px"
         />
-        {/* <Link href="/streamingPage">
-          <button className="px-24 py-3 bg-blue-500 rounded text-white font-medium">
-            Go to streaming Page
-          </button>
-        </Link> */}
       </div>
     </div>
   );
