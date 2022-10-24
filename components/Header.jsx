02d9261e8de4f179b2b86fab271b0c3b7721e9ac
IDO Majid <idomajid99@gmail.com>
2022-10-24 16:50:39 +0700

    adding getUser for authUser at header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 import Link from "next/link";
 import supabase from "../pages/api/supabase";
 
 export default function Header() {
+  const [getUser, SetGetUser] = useState(null);
+  const router = useRouter();
+
   const onSignOut = async () => {
     const { error } = await supabase.auth.signOut();
 
     if (error) {
       console.log(`${error} from signOut`);
     }
+    if (!error) {
+      router.reload();
+    }
   };
+
+  useEffect(() => {
+    const getTheUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user) {
+        SetGetUser(user);
+        console.log({ user });
+      }
+    };
+    getTheUser();
+  }, []);
 
   return (
     <div className="flex md:flex-row h-24 justify-between  items-center flex-col ">
@@ -17,23 +39,42 @@ export default function Header() {
         <Link href="/">Urologi Conference</Link>
       </div>
       <div className="flex flex-column gap-3">
-        <Link href="/signup">
-          <button className="px-4 py-1 bg-blue-500 rounded">
-            <p className="text-white   font-medium text-xs lg:text-base md:text-base">
-              Create Account
-            </p>
+        {getUser ? (
+          <Link href="/streamingPage">
+            <button className="px-4 py-1 bg-blue-500 rounded text-white font-medium text-xs sm:text-base">
+              Go to Streaming page
+            </button>
+          </Link>
+        ) : (
+          <Link href="#">
+            <button className="px-4 py-1 bg-gray-300 rounded text-white font-medium text-xs sm:text-base">
+              Go to Streaming page
+            </button>
+          </Link>
+        )}
+        {getUser ? (
+          <button
+            onClick={onSignOut}
+            className="px-4 py-1 bg-blue-500 rounded text-white font-medium text-xs sm:text-base"
+          >
+            Log Out
           </button>
-        </Link>
-        <Link href="/login">
-          <button className="px-4 py-1 bg-blue-500 rounded text-white font-medium text-xs sm:text-base">
-            Login
-          </button>
-        </Link>
-        <Link href="/streamingPage">
-          <button className="px-4 py-1 bg-gray-300 rounded text-white font-medium text-xs sm:text-base">
-            Go to Streaming page
-          </button>
-        </Link>
+        ) : (
+          <>
+            <Link href="/signup">
+              <button className="px-4 py-1 bg-blue-500 rounded">
+                <p className="text-white   font-medium text-xs lg:text-base md:text-base">
+                  Create Account
+                </p>
+              </button>
+            </Link>
+            <Link href="/login">
+              <button className="px-4 py-1 bg-blue-500 rounded text-white font-medium text-xs sm:text-base">
+                Login
+              </button>
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
