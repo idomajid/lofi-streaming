c1202a9f1f95bd5449ea652909bdff4dcf9bd98e
IDO Majid <idomajid99@gmail.com>
2022-10-25 15:00:24 +0700

    import photos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 import styles from "../styles/Container.module.css";
 import Header from "../components/Header";
 
@@ -10,11 +10,33 @@ export default function Home() {
       <div className="py-4 md:py-0">
         <Image
           className="rounded-md"
-          src="/doctor.jpg"
+          src="/lofi-image.jpg"
           alt="Picture of the author"
           width="1500px"
           height="678px"
         />
+      </div>
+      <div className="flex justify-center">
+        <div>
+          <span>Photo by </span>
+          <a
+            className="font-medium"
+            target="_blank"
+            rel="noreferrer"
+            href="https://unsplash.com/@rpnickson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          >
+            Roberto Nickson
+          </a>
+          <span> on </span>
+          <a
+            className="font-medium"
+            target="_blank"
+            rel="noreferrer"
+            href="https://unsplash.com/photos/quS7YOKdiXA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          >
+            Unsplash
+          </a>
+        </div>
       </div>
     </div>
   );
